60999d701e6db8c2c31ad00a719e36382463431b
Michał Wolanin <michalwolanin02@gmail.com>
2021-12-22 19:07:36 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import {Component, HostListener, OnInit, VERSION} from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+  title = 'portfolio';
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({behavior: 'smooth'})
+  }
+
+  firstProject(url: string) {
+    window.open(url, "http://www.psppotokwielki.pl/")
+  }
+
+  secondProject(url: string) {
+    window.open(url, "https://michalwolanin.github.io/planes/login")
+  }
+}
+
+
+
