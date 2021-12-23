9351287583e632fd847e14b1420afbc6df9c3b4d
Michał Wolanin <michalwolanin02@gmail.com>
2021-12-23 11:40:15 +0100

    Mobile version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {Component, HostListener, OnInit, VERSION} from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['../css/app.component.scss']
 })
 
 export class AppComponent {
